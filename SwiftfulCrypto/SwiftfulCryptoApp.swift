9e16491fdd37f80f8f8b617f078eab0093205df5
Evangelos Tsakanikas <evangelostsakanikas@gmail.com>
2025-01-31 14:43:04 +0100

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by user274186 on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
