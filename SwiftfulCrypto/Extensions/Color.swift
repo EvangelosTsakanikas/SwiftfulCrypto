8f4293aff55628be37cfaaa2db8ab23bc619e1a2
Evangelos Tsakanikas <evangelostsakanikas@gmail.com>
2025-01-31 18:29:25 +0100

    Updated the Color Scheme

--- SwiftfulCrypto/Extensions/Color.swift
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -21,6 +21,14 @@ struct ColorTheme {
     let secondaryText = Color("SecondaryTextColor")
 }
 
+struct ColorTheme2 {
+    let accent = Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+    let background = Color(#colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1))
+    let green = Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1))
+    let red = Color(#colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1))
+    let secondaryText = Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1))
+}
+
 struct LaunchTheme {
     let accent = Color("LaunchAccentColor")
     let background = Color("LaunchBackgroundColor")
